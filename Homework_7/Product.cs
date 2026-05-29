a6d21c4c42ae8c2146b5792d8f435457a6b31b4c
tatiad1 <tdundua1293@gmail.com>
2026-05-29 16:52:54 +0400

    Homework_7 Added!

---
+++ Homework_7/Product.cs
@@ -0,0 +1,75 @@
+﻿namespace Homework_7
+{
+    internal class Product
+    {
+        public Product()
+        {
+            reviews = new string[0];
+        }
+
+        public Product(int id, string name, string description, decimal price, int quantity, string brand, string category, decimal rating, bool isAvailable, decimal discountPercent)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Brand = brand;
+            this.Category = category;
+            this.Rating = rating;
+            this.IsAvailable = isAvailable;
+            this.DiscountPercent = discountPercent;
+            reviews = new string[0];
+        }
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public decimal Rating { get; set; }
+        public bool IsAvailable { get; set; }
+        public decimal DiscountPercent { get; set; }
+
+        private string[] reviews;
+        public string[] Reviews
+        {
+            get { return reviews; }
+        }
+
+        public void WriteReview(string review)
+        {
+            int newLen = reviews.Length + 1;
+            Array.Resize(ref reviews, newLen);
+            reviews[newLen - 1] = review;
+            Console.WriteLine($"Review \"{review}\" Added!");
+        }
+
+        public void DisplayReviews()
+        {
+            Console.WriteLine("Reviews:");
+            Console.WriteLine("------------------");
+            foreach (var review in Reviews)
+            {
+                Console.WriteLine(review);
+            }
+            Console.WriteLine("------------------");
+        }
+
+        public decimal GetDiscountedPrice()
+        {
+            decimal discountAmount = Price * (DiscountPercent / 100);
+            return Price - discountAmount;
+        }
+
+        public void DisplayInfo()
+        {
+            decimal total = GetDiscountedPrice() * Quantity;
+            Console.WriteLine($"ID: {Id}\nName: {Name}\nDescription: {Description}\nBrand: {Brand}\nQuantity: {Quantity}\nTotal: {total}");
+            DisplayReviews();
+        }
+    }
+}
---
+++ Homework_7/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Empty Constructor
+            //Product p = new Product();
+            //p1.Id = 1;
+            //p1.Name = "Keyboard";
+            //p1.Description = "Mechanical keyboard";
+            //p1.Price = 120.50m;
+            //p1.Quantity = 5;
+            //p1.Brand = "Logitech";
+            //p1.Category = "Electronics";
+            //p1.Rating = 4.7m;
+            //p1.IsAvailable = true;
+            //p1.DiscountPercent = 15m;
+            #endregion
+
+            #region Constructor
+            Product p = new Product(1, "Keyboard", "Mechanical keyboard", 120.50m, 5, "Logitech", "Electronics", 4.7m, true, 15m);
+
+            p.WriteReview("Nice Looking");
+            p.DisplayInfo();
+
+            #endregion
+        }
+    }
+}
