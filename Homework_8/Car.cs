51b9bc7fcd0b5ac43d05b73366c8e9cf7caf5c39
tatiad1 <tdundua1293@gmail.com>
2026-06-02 20:06:18 +0400

    Homework_8

---
+++ Homework_8/Car.cs
@@ -0,0 +1,120 @@
+﻿namespace Homework_8
+{
+    internal class Car
+    {
+        private int id;
+        private string brand;
+        private string model;
+        private int year;
+        private decimal price;
+        private string color;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (value > 0)
+                {
+                    id = value;
+                }
+            }
+        }
+
+        public string Brand
+        {
+            get { return brand; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    brand = value;
+                }
+            }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    model = value;
+                }
+            }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value >= 1886 && value <= DateTime.Now.Year)
+                {
+                    year = value;
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (value >= 0)
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    color = value;
+                }
+            }
+        }
+
+        public Car(int id, string brand, string model, int year, decimal price, string color)
+        {
+            this.Id = id;
+            this.Brand = brand;
+            this.Model = model;
+            this.Year = year;
+            this.Price = price;
+            this.Color = color;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {Id} | {Brand} {Model} | {Year} | {Price} | {Color}");
+        }
+
+        public string GetFullName()
+        {
+            return $"{Brand} - {Model}";
+        }
+
+        public int GetAge()
+        {
+            return DateTime.Now.Year - Year;
+        }
+
+        public bool IsLuxury()
+        {
+            return Price >= 80000;
+        }
+
+        public void ChangeColor(string newColor)
+        {
+            Color = newColor;
+        }
+    }
+
+}
---
+++ Homework_8/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Homework_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\user\Desktop\IT_Academy_Course\IT_Academy_Course\Homework_8\CarsData.txt");
+
+            Car[] cars = new Car[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] data = lines[i].Split(',');
+                bool isValidYear = int.TryParse(data[2], out int year);
+                bool isValidPrice = decimal.TryParse(data[3], out decimal price);
+
+                if (data.Length == 5 && isValidYear && isValidPrice)
+                {
+                    cars[i] = new Car(
+                        i + 1,
+                        data[0],
+                        data[1],
+                        year,
+                        price,
+                        data[4]
+                    );
+                }
+            }
+            Console.WriteLine("All Cars:");
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i].DisplayInfo();
+            }
+
+            Console.WriteLine("\nExample Methods:");
+            Console.WriteLine();
+
+            Console.WriteLine($"Full Name: {cars[0].GetFullName()}");
+            Console.WriteLine($"Age: {cars[0].GetAge()}");
+            Console.WriteLine($"Luxury: {cars[0].IsLuxury()}");
+
+            cars[0].ChangeColor("Black");
+
+            Console.WriteLine("\nAfter Color Change:");
+            cars[0].DisplayInfo();
+        }
+    }
+}
