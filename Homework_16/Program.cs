ad56bc12709c605f43d3482065ee4334ae51790b
tatiad1 <tdundua1293@gmail.com>
2026-06-28 18:31:11 +0400

    Homework_16 Added!

---
+++ Homework_16/Program.cs
@@ -0,0 +1,96 @@
+﻿using Homework_16.Services;
+
+namespace Homework_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>()
+            {
+                5, 2, 8, 2, 10, 5, 1
+            };
+
+            Console.WriteLine("===== WHERE =====");
+            var evenNumbers = Algorithms.Where(numbers, x => x % 2 == 0);
+
+            foreach (var item in evenNumbers)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("===== ORDER BY =====");
+            var sorted = Algorithms.OrderBy(numbers, x => x);
+
+            foreach (var item in sorted)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("===== FIRST =====");
+            Console.WriteLine(Algorithms.First(numbers, x => x > 4));
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== FIRST OR DEFAULT =====");
+            Console.WriteLine(Algorithms.FirstOrDefault(numbers, x => x > 100));
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== SINGLE =====");
+
+            try
+            {
+                Console.WriteLine(Algorithms.Single(numbers, x => x == 10));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== SINGLE OR DEFAULT =====");
+
+            try
+            {
+                Console.WriteLine(Algorithms.SingleOrDefault(numbers, x => x == 100));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== ANY =====");
+            Console.WriteLine(Algorithms.Any(numbers, x => x > 8));
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== ALL =====");
+            Console.WriteLine(Algorithms.All(numbers, x => x > 0));
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== COUNT =====");
+            Console.WriteLine(Algorithms.Count(numbers, x => x == 5));
+
+            Console.WriteLine();
+
+            Console.WriteLine("===== DISTINCT =====");
+            var unique = Algorithms.Distinct(numbers);
+
+            foreach (var item in unique)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Homework_16/Services/Algorithms.cs
@@ -0,0 +1,157 @@
+﻿namespace Homework_16.Services
+{
+    internal class Algorithms
+    {
+        public static IEnumerable<T> Where<T>(IEnumerable<T> collection, Predicate<T> pred)
+        {
+            foreach (T item in collection)
+            {
+                if (pred(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+        public static IEnumerable<T> OrderBy<T, TKey>(IEnumerable<T> collection, Func<T, TKey> keySelector) where TKey : IComparable<TKey>
+        {
+            List<T> list = collection.ToList();
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (keySelector(list[i]).CompareTo(keySelector(list[j])) > 0)
+                    {
+                        T temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+
+            return list;
+        }
+        public static T First<T>(IEnumerable<T> collection, Predicate<T> pred)
+        {
+            foreach (T item in collection)
+            {
+                if (pred(item))
+                {
+                    return item;
+                }
+            }
+
+            throw new Exception("Sequence contains no such element.");
+        }
+        public static T FirstOrDefault<T>(IEnumerable<T> collection, Predicate<T> pred)
+        {
+            foreach (T item in collection)
+            {
+                if (pred(item))
+                {
+                    return item;
+                }
+            }
+
+            return default(T);
+        }
+        public static T Single<T>(IEnumerable<T> collection, Predicate<T> pred)
+        {
+            bool found = false;
+            T result = default;
+
+            foreach (T item in collection)
+            {
+                if (pred(item))
+                {
+                    if (found)
+                    {
+                        throw new Exception("More than one matching element.");
+                    }
+
+                    found = true;
+                    result = item;
+                }
+            }
+
+            if (!found)
+            {
+                throw new Exception("No matching element.");
+            }
+
+            return result;
+        }
+        public static T SingleOrDefault<T>(IEnumerable<T> collection, Predicate<T> pred)
+        {
+            bool found = false;
+            T result = default;
+
+            foreach (T item in collection)
+            {
+                if (pred(item))
+                {
+                    if (found)
+                    {
+                        throw new Exception("More than one matching element.");
+                    }
+
+                    found = true;
+                    result = item;
+                }
+            }
+
+            return result;
+        }
+        public static bool Any<T>(IEnumerable<T> collection, Predicate<T> pred)
+        {
+            foreach (T item in collection)
+            {
+                if (pred(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public static bool All<T>(IEnumerable<T> collection, Predicate<T> pred)
+        {
+            foreach (T item in collection)
+            {
+                if (!pred(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static int Count<T>(IEnumerable<T> collection, Predicate<T> pred)
+        {
+            int count = 0;
+            foreach (T item in collection)
+            {
+                if (pred(item))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static IEnumerable<T> Distinct<T>(IEnumerable<T> collection)
+        {
+            List<T> unique = new List<T>();
+
+            foreach (T item in collection)
+            {
+                if (!unique.Contains(item))
+                {
+                    unique.Add(item);
+                    yield return item;
+                }
+            }
+        }
+    }
+}
